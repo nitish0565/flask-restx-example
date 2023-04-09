75ebf521bae37458022255b242058911e6c9bb3c
Nitish Reddy Singidi <nsingidi@randomtrees.com>
2023-04-09 17:12:31 +0530

    flask-restx init setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from app import create_app
+
+
+app1 = create_app()
+
+
+
+
+if __name__ == '__main__':
+    app1.run(debug=True, host='0.0.0.0', port=5001)
---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+
+    app.config['JSON_SORT_KEYS'] = False
+
+    return app
+
